098d8b7ac0ad5bcad637041e402726794c7e4b9d
lucifer-code <56485755+lucifer-code@users.noreply.github.com>
2021-11-07 20:09:25 +0530

    -

---
+++ AREA of circle.cpp
@@ -0,0 +1,36 @@
+
+
+
+#include <iostream>
+#define pi 3.1417
+using namespace std;
+int r;
+int main()
+{   cout<<"enter the radius\n";
+
+    cin>>r;
+    cout<<"area of circle is "<<pi*r*r;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
